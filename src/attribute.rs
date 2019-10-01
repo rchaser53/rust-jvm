d0ddac650ca16ef98e00fce10b7dc3dea97711b4
rchaser53 <tayoshizawa29@gmail.com>
2019-10-01 21:01:43 +0900

    Add Instruction and Code

--- src/attribute.rs
+++ src/attribute.rs
@@ -6,7 +6,7 @@ pub enum Attribute {
     SourceDebugExtension,
     BootstrapMethods,
     ConstantValue,
-    Code,
+    Code(Code),
     Exceptions,
     RuntimeVisibleParameterAnnotations,
     RuntimeInvisibleParameterAnnotations,
@@ -30,6 +30,35 @@ pub struct SourceFile {
     attribute_name_index: u16, // u2
     attribute_length: u32,     // u4
     sourcefile_index: u16,     // u2
+}
+
+#[derive(Debug)]
+pub struct Code {
+    attribute_name_index: u16, // u2
+    attribute_length: u32,     // u4
+    max_stack: u16,            // u2
+    max_locals: u16,           // u2
+    code_length: u32,          // u4
+    code: Vec<Instruction>,
+    exception_table_length: u16, // u2
+    exception_table: Vec<ExceptionTableItem>,
+    attributes_count: u16, // u2
+    attribute_info: Vec<Attribute>,
+}
+
+#[derive(Debug)]
+pub struct ExceptionTableItem {
+    pub start_pc: u16,   //u2
+    pub end_pc: u16,     //u2
+    pub handler_pc: u16, //u2
+    pub catch_type: u16, //u2
+}
+
+#[derive(Debug)]
+pub enum Instruction {
+    Aload(usize),
+    Invokespecial(u16, u16),
+    Return,
 }
 
 #[derive(Debug)]
