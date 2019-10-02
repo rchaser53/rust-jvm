284a072cf904564e2c4ab3a3be39f1de0bfd0811
rchaser53 <tayoshizawa29@gmail.com>
2019-10-03 00:42:48 +0900

    add the detail for Field

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,47 @@ use crate::utils::read_file;
 
 #[derive(Debug)]
 struct Interface;
+
 #[derive(Debug)]
-struct Field;
+struct Field {
+    pub access_flags: FieldAccessFlag, // u2
+    pub name_index: u16,               // u2
+    pub descriptor_index: u16,         // u2
+    pub attributes_count: u16,         // u2
+    pub attribute_info: Vec<Attribute>,
+}
+
+#[derive(Debug)]
+pub enum FieldAccessFlag {
+    Unknown,
+    AccPublic,
+    AccPrivate,
+    AccProtected,
+    AccStatic,
+    AccFinal,
+    AccVolatitle,
+    AccTransient,
+    AccSynthetic,
+    AccEnum,
+}
+
+impl From<u16> for FieldAccessFlag {
+    fn from(num: u16) -> FieldAccessFlag {
+        match num {
+            0x0000 => FieldAccessFlag::Unknown,       // custom
+            0x0001 => FieldAccessFlag::AccPublic,
+            0x0002 => FieldAccessFlag::AccPrivate,
+            0x0004 => FieldAccessFlag::AccProtected,
+            0x0008 => FieldAccessFlag::AccStatic,
+            0x0010 => FieldAccessFlag::AccFinal,
+            0x0040 => FieldAccessFlag::AccVolatitle,
+            0x0080 => FieldAccessFlag::AccTransient,
+            0x1000 => FieldAccessFlag::AccSynthetic,
+            0x4000 => FieldAccessFlag::AccEnum,
+            _ => panic!("failed to convert {} to FieldAccessFlag", num),
+        }
+    }
+}
 
 #[derive(Debug)]
 struct Method {
@@ -31,7 +70,7 @@ struct ClassFile {
     pub major_version: u16,         // u2
     pub constant_pool_count: u16,   // u2
     pub cp_info: ConstantPool,      // cp_info        constant_pool[constant_pool_count-1];
-    pub access_flags: u16,          // u2
+    pub access_flags: AccessFlag,   // u2
     pub this_class: u16,            // u2
     pub super_class: u16,           // u2
     pub interfaces_count: u16,      // u2
