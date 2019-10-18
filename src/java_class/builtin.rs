683a976073410e588d8e127468c49b4497e95dce
rchaser53 <tayoshizawa29@gmail.com>
2019-10-18 23:00:21 +0900

    add BuiltInMethod

--- src/java_class/builtin.rs
+++ src/java_class/builtin.rs
@@ -1,4 +1,7 @@
 use std::collections::HashMap;
+use std::fmt;
+
+use crate::operand::{OperandStack, OperandStackItem};
 
 #[derive(Debug)]
 pub struct BuiltIn<'a> {
@@ -7,4 +10,24 @@ pub struct BuiltIn<'a> {
 }
 
 #[derive(Debug)]
-pub struct BuiltInMethod;
+pub struct BuiltInMethod {
+    pub name: String,
+    pub descriptor_index: String,
+    pub code: BuitlInCodeType,
+    pub max_locals: usize,
+}
+
+impl BuiltInMethod {
+    pub fn execute(&mut self, operand_stack: &mut OperandStack) {
+        match self.code {
+            BuitlInCodeType::Println => {
+                println!("test!");
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum BuitlInCodeType {
+    Println,
+}
