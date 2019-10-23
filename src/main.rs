a97e1f861bcd536cb830be5452c584ac65435c2f
rchaser53 <tayoshizawa29@gmail.com>
2019-10-23 21:27:29 +0900

    add clap to read file

--- src/main.rs
+++ src/main.rs
@@ -21,17 +21,30 @@ use crate::java_class::{
 use crate::utils::read_file;
 extern crate lazy_static;
 
+extern crate clap;
+use clap::App;
+
 use std::collections::HashMap;
 
 fn main() {
-    let class_name = "HelloWorld.class";
-    if let Ok(buffer) = read_file(class_name, &mut vec![]) {
-        let (class_file, _pc_count) = Custom::new(buffer, 0);
-        let class_map = setup_class_map();
-        let mut context = Context::new(class_map);
-        context.run_entry_file(class_file);
+    let matches = App::new("rj")
+        .version("0.1")
+        .author("rchaser53 <tayoshizawa29@gmail.com>")
+        .about("toy jvm implemented by Rust")
+        .args_from_usage("<INPUT>              'Sets the input file to use'")
+        .get_matches();
+    if let Some(file_name) = matches.value_of("INPUT") {
+        let class_name = file_name.to_string() + ".class";
+        if let Ok(buffer) = read_file(&class_name, &mut vec![]) {
+            let (class_file, _pc_count) = Custom::new(buffer, 0);
+            let class_map = setup_class_map();
+            let mut context = Context::new(class_map);
+            context.run_entry_file(class_file);
+        } else {
+            unimplemented!("need to add handler for the case failed to find the class file")
+        }
     } else {
-        unimplemented!("need to add handler for the case failed to find the class file")
+        println!("should input the file");
     }
 }
 
