ea320fea69ee5b72e892cf451157acc00c3cbc12
rchaser53 <tayoshizawa29@gmail.com>
2019-10-25 22:48:52 +0900

    add test

--- tests/test.rs
+++ tests/test.rs
@@ -3,4 +3,5 @@ use r_jvm;
 fn main() {
     r_jvm::execute(String::from("tests/class/HelloWorld"), false);
     r_jvm::execute(String::from("tests/class/FizzBuzz"), false);
+    r_jvm::execute(String::from("tests/class/FizzBuzz2"), false);
 }
