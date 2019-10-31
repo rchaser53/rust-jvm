afc7eb043799ca389aaed3a6358aa8da1773c469
rchaser53 <tayoshizawa29@gmail.com>
2019-11-01 00:10:56 +0900

    add switch test

---
+++ tests/original/Switch.java
@@ -0,0 +1,17 @@
+public class Switch {
+  public static void main(String[] args){
+    int i = 1;
+    switch (i) {
+      case 1:
+        System.out.println("abc");
+        break;
+      case 2:
+        System.out.println("def");
+        break;
+      default:
+        System.out.println("def");
+        break;
+    }
+  }
+}
+
--- tests/test.rs
+++ tests/test.rs
@@ -6,4 +6,5 @@ fn main() {
     r_jvm::execute(String::from("tests/class/FizzBuzz2"), 0);
     r_jvm::execute(String::from("tests/class/NewAndCallInstanceMethod"), 0);
     r_jvm::execute(String::from("tests/class/InitializeStatic"), 0);
+    r_jvm::execute(String::from("tests/class/Switch"), 0);
 }
